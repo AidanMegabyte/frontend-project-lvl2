abe1590232695724794b486e04ac67b3fc8e54e8
Sergei Kuskov <aidan.megabyte@gmail.com>
2021-02-17 00:03:06 +0300

    Модификация вывода --help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,15 @@ import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
 
+program.configureHelp({
+  sortOptions: true,
+});
+
 program
-  .description('Compares two configuration files and shows a difference.');
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
