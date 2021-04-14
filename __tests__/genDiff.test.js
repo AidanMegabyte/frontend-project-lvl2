2f95fab3918cb5dc0030f0347527d53ad6026a2b
Sergei Kuskov <aidan.megabyte@gmail.com>
2021-04-15 01:25:43 +0300

    [*] Устранение дубликатов в коде v3.0

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,38 +3,40 @@ import { promises as fsp } from 'fs';
 import { getFixturePath } from './common.js';
 import genDiff from '../src/index.js';
 
-test('Empty JSONs diff generation', async () => {
-  const diffExpected = (await fsp.readFile(getFixturePath('genDiff/json/empty/diff.txt'), 'utf-8')).slice(0, -1);
-  const diffActual = genDiff(
-    getFixturePath('genDiff/json/empty/1.json'),
-    getFixturePath('genDiff/json/empty/2.json'),
-  );
+const checkDiff = async (filePath1, filePath2, diffPath) => {
+  const diffExpected = (await fsp.readFile(getFixturePath(diffPath), 'utf-8')).slice(0, -1);
+  const diffActual = genDiff(getFixturePath(filePath1), getFixturePath(filePath2));
   expect(diffActual).toEqual(diffExpected);
+};
+
+test('Empty JSONs diff generation', async () => {
+  await checkDiff(
+    'genDiff/json/empty/1.json',
+    'genDiff/json/empty/2.json',
+    'genDiff/json/empty/diff.txt',
+  );
 });
 
 test('Plain JSONs diff generation', async () => {
-  const diffExpected = (await fsp.readFile(getFixturePath('genDiff/json/plain/diff.txt'), 'utf-8')).slice(0, -1);
-  const diffActual = genDiff(
-    getFixturePath('genDiff/json/plain/1.json'),
-    getFixturePath('genDiff/json/plain/2.json'),
+  await checkDiff(
+    'genDiff/json/plain/1.json',
+    'genDiff/json/plain/2.json',
+    'genDiff/json/plain/diff.txt',
   );
-  expect(diffActual).toEqual(diffExpected);
 });
 
 test('Empty YAMLs diff generation', async () => {
-  const diffExpected = (await fsp.readFile(getFixturePath('genDiff/yaml/empty/diff.txt'), 'utf-8')).slice(0, -1);
-  const diffActual = genDiff(
-    getFixturePath('genDiff/yaml/empty/1.yaml'),
-    getFixturePath('genDiff/yaml/empty/2.yaml'),
+  await checkDiff(
+    'genDiff/yaml/empty/1.yaml',
+    'genDiff/yaml/empty/2.yaml',
+    'genDiff/yaml/empty/diff.txt',
   );
-  expect(diffActual).toEqual(diffExpected);
 });
 
 test('Plain YAMLs diff generation', async () => {
-  const diffExpected = (await fsp.readFile(getFixturePath('genDiff/yaml/plain/diff.txt'), 'utf-8')).slice(0, -1);
-  const diffActual = genDiff(
-    getFixturePath('genDiff/yaml/plain/1.yaml'),
-    getFixturePath('genDiff/yaml/plain/2.yaml'),
+  await checkDiff(
+    'genDiff/yaml/plain/1.yaml',
+    'genDiff/yaml/plain/2.yaml',
+    'genDiff/yaml/plain/diff.txt',
   );
-  expect(diffActual).toEqual(diffExpected);
 });
